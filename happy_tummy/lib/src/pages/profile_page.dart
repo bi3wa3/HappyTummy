d0f7ed22666fedf6532610f0b95adabff0f2cd64
unknown <bi3wa3@gmail.com>
2020-07-26 13:24:13 +0545

    profile page

--- happy_tummy/lib/src/pages/profile_page.dart
+++ happy_tummy/lib/src/pages/profile_page.dart
@@ -9,32 +9,186 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  final _auth = FirebaseAuth.instance;
-  FirebaseUser loggedInUser;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
 
-    getCurrentUser();
-  }
-
-  void getCurrentUser() async {
-    try{
-      final user = await _auth.currentUser();
-      if (user != null){
-        loggedInUser = user;
-        print(loggedInUser);
-      }
-    }catch(e){
-      print(e);
-    }
-  }
+  // void getCurrentUser() async {
+  //   try{
+  //     final user = await _auth.currentUser();
+  //     if (user != null){
+  //       loggedInUser = user;
+  //       print(loggedInUser);
+  //     }
+  //   }catch(e){
+  //     print(e);
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page'),
+    // return Center(
+    //   child: Text('Profile Page'),
+    // );
+
+     return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(10.0,0,10.0,0),
+
+        child: ListView(
+          children: <Widget>[
+          //   Container(
+          // padding: EdgeInsets.symmetric(horizontal: 20.0),
+          // child: Column(
+          //   crossAxisAlignment: CrossAxisAlignment.start,
+          //   children: <Widget>[
+              Text(
+                
+                "Profile",
+                style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                  
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 120.0,
+                    width: 120.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(60.0),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 3.0,
+                            offset: Offset(0, 4.0),
+                            color: Colors.black38),
+                      ],
+                      image: DecorationImage(
+                        image: AssetImage(
+                          "assets/images/suyog.jpg",
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              
+            Divider(),
+            Container(height: 15.0),
+
+            Padding(
+              padding: EdgeInsets.all(5.0),
+              child: Text(
+                "Account Information".toUpperCase(),
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+            ListTile(
+              title: Text(
+                "Full Name",
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+
+              subtitle: Text(
+                "suyog guragain",
+              ),
+
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.edit,
+                  size: 20.0,
+                ),
+                onPressed: (){
+                },
+                tooltip: "Edit",
+              ),
+            ),
+
+            ListTile(
+              title: Text(
+                "Email",
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+
+              subtitle: Text(
+                "roysuyog@gmail.com",
+              ),
+            ),
+
+            ListTile(
+              title: Text(
+                "Phone",
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+
+              subtitle: Text(
+                "987654321",
+              ),
+            ),
+
+            ListTile(
+              title: Text(
+                "Address",
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+
+              subtitle: Text(
+                "Balkumari,lalitpur",
+              ),
+            ),
+
+            ListTile(
+              title: Text(
+                "Gender",
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+
+              subtitle: Text(
+                "Male",
+              ),
+            ),
+
+            ListTile(
+              title: Text(
+                "Date of Birth",
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+
+              subtitle: Text(
+                "magh 4, 2054",
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
